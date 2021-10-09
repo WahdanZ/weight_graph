5066c49d13d3002987ce100279da6f0758dff1e1
Ahmed Wahdan <ahmed.wahdan@outlook.com>
2021-10-09 14:39:52 +0200

    init

---
+++ android/app/src/main/kotlin/com/wahdan/weight_graph/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.wahdan.weight_graph
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/feature/ui/widgets/weight_graph_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WeightGraphWidget extends StatefulWidget {
+  const WeightGraphWidget({Key? key}) : super(key: key);
+
+  @override
+  _WeightGraphWidgetState createState() => _WeightGraphWidgetState();
+}
+
+class _WeightGraphWidgetState extends State<WeightGraphWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
